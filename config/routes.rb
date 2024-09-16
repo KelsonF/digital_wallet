81cc5626158efab31d516ef29543b7754fa4b0dc
Kelson Eduardo <kfeduardo2003@gmail.com>
2024-09-16 09:09:11 -0300

    feat: add the new login and register routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get "registrations/create"
+  get "sessions/create"
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :transactions
 
-  post "/login", to: "auth#login"
-  post "/register", to: "auth#register"
+  post "/login", to: "sessions#create"
+  post "/register", to: "registration#create"
 end
